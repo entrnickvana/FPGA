d8920a2649e0e46c799b427b7b47f12653aac0dd
nick elliott <Nick.jwl123@gmail.com>
2017-08-26 00:43:46 -0600

    first commit

---
+++ assign1/led_Mod.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:33:02 08/26/2017 
+// Design Name: 
+// Module Name:    led_Mod 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module led_Mod(
+	input clock,
+	output pins
+    );
+
+	always @(posedge clk or posedge rst) 
+	begin
+		if (rst) 
+		begin
+			// reset
+			
+		end
+		else if () 
+		begin
+			
+		end
+	end
+
+
+endmodule
