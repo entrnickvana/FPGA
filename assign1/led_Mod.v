7b063ab8890328ae2b96b6aeecc4c8743ea61140
nick elliott <Nick.jwl123@gmail.com>
2017-08-26 00:51:05 -0600

    Basic Comments Added To led_Mod.v

--- assign1/led_Mod.v
+++ assign1/led_Mod.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: JWL Industries
+// Engineer: Nicholas Elliott
 // 
 // Create Date:    00:33:02 08/26/2017 
 // Design Name: 
 // Module Name:    led_Mod 
 // Project Name: 
-// Target Devices: 
+// Target Devices: Spartan 6
 // Tool versions: 
 // Description: 
 //
