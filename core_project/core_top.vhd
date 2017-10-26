27a6a2deac2221d0073e05c33b525892711b027a
nick elliott <Nick.jwl123@gmail.com>
2017-10-26 10:53:59 -0600

    Push from NICK, beginning of core: core.v, and top level of core: core_top.v

---
+++ core_project/core_top.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:19:19 10/26/2017 
+-- Design Name: 
+-- Module Name:    core_top - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity core_top is
+end core_top;
+
+architecture Behavioral of core_top is
+
+begin
+
+
+end Behavioral;
+
